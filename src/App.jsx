655e0def6c70a0aaf1addbb546e4fab60411802a
Marte <125874444+mar-ols@users.noreply.github.com>
2024-09-23 13:48:42 +0200

    Adds routing to and single product page content with styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { Layout } from "./components/layout";
 import { Home } from "./pages/home";
+import { DisplayProduct } from "./pages/single-product";
 import "./styles/App.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path=":id" element={<DisplayProduct />} />
           <Route path="*" element={<div>Route not found</div>} />
         </Route>
       </Routes>
