341e45632511dd6eee3e78de5ce5ffae8291ad5c
Marte <125874444+mar-ols@users.noreply.github.com>
2024-09-30 18:50:13 +0200

    Adds cart page with styling

---
+++ src/styles/styled-components/cart/cartContainer.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const StyledCartContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export { StyledCartContainer };
---
+++ src/styles/styled-components/centered-div.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  max-width: 75%;
+  margin: auto;
+  padding-bottom: 2rem;
+
+  @media (min-width: 768px) {
+    max-width: 55%;
+  }
+`;
+
+export { StyledDiv };
