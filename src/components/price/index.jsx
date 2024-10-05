c591e715b0e4fe640c1ea2a547d8af0a1bcbc9d6
Marte <125874444+mar-ols@users.noreply.github.com>
2024-10-05 14:46:22 +0200

    Changes classname

--- src/components/price/index.jsx
+++ src/components/price/index.jsx
@@ -8,7 +8,7 @@ function DisplayPrice(discountPrice, price) {
       ) : (
         <div className="bold">
           <p>
-            <span className="discountedPrice">{discountPrice}KR</span>{" "}
+            <span className="warn">{discountPrice}KR</span>{" "}
             <span className="oldPrice">{price}KR</span>
           </p>
           <p>Save {priceDifference.toFixed(2)}KR</p>
