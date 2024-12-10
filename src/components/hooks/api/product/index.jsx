7864ea1c3be78ef4451cb69f73df0b7efbb44475
Marte <125874444+mar-ols@users.noreply.github.com>
2024-12-10 17:18:50 +0100

    Adds jdocs comment and base api import to single product hook

--- src/components/hooks/api/product/index.jsx
+++ src/components/hooks/api/product/index.jsx
@@ -1,5 +1,18 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { BASE_API } from "../../../../common/constants";
+
+/**
+ * Custom React hook to fetch and manage the state of a specific product's data from the API.
+ *
+ * This hook fetches data for a product identified by its `id` from the URL parameters
+ * using the `useParams` hook from React Router. It manages loading, error, and data states.
+ *
+ * @returns {Object} An object containing the following:
+ * - `data` (Object|null): The fetched product data, or `null` if not yet loaded.
+ * - `isLoading` (boolean): `true` if the data is currently being fetched, `false` otherwise.
+ * - `isError` (boolean): `true` if an error occurred during data fetching, `false` otherwise.
+ */
 
 function useProduct() {
   const [data, setData] = useState(null);
@@ -13,9 +26,7 @@ function useProduct() {
         setIsLoading(true);
         setIsError(false);
 
-        const fetchedData = await fetch(
-          `https://v2.api.noroff.dev/online-shop/${id}`
-        );
+        const fetchedData = await fetch(`${BASE_API}/${id}`);
         const json = await fetchedData.json();
 
         setData(json.data);
