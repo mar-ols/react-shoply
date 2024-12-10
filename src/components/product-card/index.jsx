1b33ba025b7fef0185b879f98d1475d492b1a1b1
Marte <125874444+mar-ols@users.noreply.github.com>
2024-12-10 17:19:13 +0100

    Adds api import to product card

--- src/components/product-card/index.jsx
+++ src/components/product-card/index.jsx
@@ -3,11 +3,10 @@ import { useProducts } from "../hooks/api/products";
 import { Link } from "react-router-dom";
 import { StyledButton } from "../../styles/styled-components/buttons";
 import { DisplayPrice } from "../price";
+import { BASE_API } from "../../common/constants";
 
 function ProductCard() {
-  const { data, isLoading, isError } = useProducts(
-    "https://v2.api.noroff.dev/online-shop"
-  );
+  const { data, isLoading, isError } = useProducts(`${BASE_API}`);
 
   if (isLoading) {
     return <div>Loading</div>;
@@ -18,24 +17,22 @@ function ProductCard() {
   }
 
   return (
-    <>
-      <StyledProductCardContainer>
-        {data.map((product) => (
-          <div key={product.id} className="productCard">
-            <Link to={product.id}>
-              <img src={product.image.url} alt={product.title} />
-            </Link>
-            <Link to={product.id} className="bold">
-              {product.title}
-            </Link>
-            {DisplayPrice(product.discountedPrice, product.price)}
-            <Link to={product.id}>
-              <StyledButton>View Product</StyledButton>
-            </Link>
-          </div>
-        ))}
-      </StyledProductCardContainer>
-    </>
+    <StyledProductCardContainer>
+      {data.map((product) => (
+        <div key={product.id} className="productCard">
+          <Link to={product.id}>
+            <img src={product.image.url} alt={product.title} />
+          </Link>
+          <Link to={product.id} className="bold">
+            {product.title}
+          </Link>
+          {DisplayPrice(product.discountedPrice, product.price)}
+          <Link to={product.id}>
+            <StyledButton>View Product</StyledButton>
+          </Link>
+        </div>
+      ))}
+    </StyledProductCardContainer>
   );
 }
 
