12b9b0f798a10f4403f3daa5d87c2c8e722b810d
Marte <125874444+mar-ols@users.noreply.github.com>
2024-12-10 17:19:30 +0100

    Adds jdocs comment to products hook

--- src/components/hooks/api/products/index.jsx
+++ src/components/hooks/api/products/index.jsx
@@ -1,5 +1,16 @@
 import { useState, useEffect } from "react";
 
+/**
+ * Custom React hook to fetch and manage the state of a list of products from the provided API endpoint.
+ *
+ * This hook fetches data from the specified url and manages loading, error, and data states.
+ *
+ * @param {string} url - The API endpoint to fetch the products from.
+ * @returns {Object} An object containing the following:
+ * - `data` (Array): The array of fetched product data, empty if no data has been loaded yet.
+ * - `isLoading` (boolean): `true` if the data is currently being fetched, `false` otherwise.
+ * - `isError` (boolean): `true` if an error occurred during data fetching, `false` otherwise.
+ */
 function useProducts(url) {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
